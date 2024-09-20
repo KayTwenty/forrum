f7c43f68b379bc4771ac1039d8bd1ac6d7234425
David Anton <davidanton037@gmail.com>
2024-09-19 21:30:10 -0400

    Add basic theme for styles

---
+++ src/chakra/theme.ts
@@ -0,0 +1,26 @@
+import "@fontsource/open-sans/300.css";
+import "@fontsource/open-sans/400.css";
+import "@fontsource/open-sans/700.css";
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+    colors: {
+        brand: {
+            100: "#FF0000",
+        },
+    },
+    fonts: {
+        // Default font family
+        body: "Open Sans, sans-serif",
+    },
+    styles: {
+        global: () => ({
+            body: {
+                bg: "gray.200",
+            },
+        }),
+    },
+    components: {
+        // UI components
+    }
+});
