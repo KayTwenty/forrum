aa89f218a23d9326d8bbf66f7238c3a0c3f70de0
David Anton <davidanton037@gmail.com>
2024-09-19 21:21:34 -0400

    Wrap app with Chakra UI

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Home</title>
+            </Head>
+            <h1>Welcome to Next.js</h1>
+        </>
+    )
+}
